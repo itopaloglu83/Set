2893ae2f07fb85540ee2160aa256c4fa962b076d
Ihsan TOPALOGLU <itopaloglu83@gmail.com>
2021-07-26 22:25:45 -0400

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by İhsan TOPALOĞLU on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
